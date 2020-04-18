20d307e2494c13ca719930e2317438fd6c267973
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-04-18 17:01:58 -0300

    Adicionado validaçao para campos obrigatorios nos metodos dos controllers Session e User.

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import authConfig from '../../config/auth';
 
@@ -6,6 +7,14 @@ import User from '../models/User';
 
 class SessionController {
   async store(request, response) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails' });
+    }
     const { email, password } = request.body;
 
     const user = await User.findOne({
@@ -16,7 +25,7 @@ class SessionController {
       return response.status(401).json({ error: 'User not found' });
     }
 
-    if (!user.checkPassword(password)) {
+    if (!(await user.checkPassword(password))) {
       return response.status(401).json({ error: 'Password does not match' });
     }
 
