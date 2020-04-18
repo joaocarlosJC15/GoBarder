cd67af5992074c18f3cea118d0b24f8ae61f24f9
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-04-18 12:39:48 -0300

    Configurando a conexao dos models com o banco de dados.

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
+
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    name: 'João Carlos Alvim',
+    email: 'jc14joaocarlos@gmail.com',
+    password_hash: '12345',
+  });
+
+  return response.json(user);
+});
 
 export default routes;
