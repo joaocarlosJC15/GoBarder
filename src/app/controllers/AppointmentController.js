699f46c6efea3c4ca95f939853e90d2f2b2728a7
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-04-21 14:10:16 -0300

    Implementado cadastro de appointments.

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,62 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class AppointmentController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { provider_id, date } = request.body;
+
+    const checkIsProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return response
+        .status(401)
+        .json({ errpr: 'You can only create appointments with providers' });
+    }
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return response
+        .status(400)
+        .json({ error: 'Past dates are not permitted' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return response
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: request.userId,
+      provider_id,
+      date: hourStart,
+    });
+
+    return response.json(appointment);
+  }
+}
+
+export default new AppointmentController();
