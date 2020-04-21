afe75c99cacc70c693ff949c9fe9139ee701bc55
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-04-21 11:25:45 -0300

    Implementada logica para listagem de providers.

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionControler from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,11 +14,14 @@ const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
+
 routes.post('/sessions', SessionControler.store);
 
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
